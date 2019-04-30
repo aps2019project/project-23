1869b585931a220d60a062e593b81d8620caf5ff
Mohammad Shojaeyan <mshojaeyan@ce.sharif.edu>
2019-05-01 01:13:24 +0430

    Class deck

---
+++ APProject/src/Deck.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Deck {
+
+    private String name;
+    private ArrayList<Card> deckCard = new ArrayList<Card>();
+
+    public Deck(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Card> getDeckCard() {
+        return deckCard;
+    }
+
+    public void addCard(Card card){
+        deckCard.add(card);
+    }
+
+    public int indexOfCardInDeck(String cardID) {
+        for ( int i = 0 ; i < deckCard.size() ; i ++ ) {
+            if ( deckCard.get(i).cardID.matches(cardID))
+                return i;
+        }
+        return -1;
+    }
+
+    public int counterOfCard(){
+        int counter = 0;
+        for ( int i = 0 ; i < deckCard.size() ; i ++ ) {
+            if (deckCard.get(i) instanceof Item)
+                continue;
+            else if (deckCard.get(i) instanceof Hero )
+                continue;
+            counter++;
+        }
+        return counter;
+    }
+
+    public boolean existHeroInDeck(){
+        for ( int i = 0 ; i < deckCard.size() ; i ++ ) {
+            if (deckCard.get(i) instanceof Hero )
+                return true;
+        }
+        return false;
+    }
+
+    public void deleteCard(String cardID) {
+        deckCard.remove(indexOfCardInDeck(cardID));
+    }
+
+}
