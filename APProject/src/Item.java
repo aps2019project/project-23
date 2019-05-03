9149bdc11ea3fedf33a8b54063bafd06e6a8bd14
Mohammad Shojaeyan <mshojaeyan@ce.sharif.edu>
2019-05-03 23:27:52 +0430

    Some change in item

--- APProject/src/Item.java
+++ APProject/src/Item.java
@@ -5,6 +5,7 @@ public class Item extends Card {
     private String desc;
     private boolean collectable;
     private static ArrayList<Item> collectableItems = new ArrayList<Item>();
+    private String effect;
 
     public static ArrayList<Item> getCollectableItems() {
         return collectableItems;
@@ -14,11 +15,15 @@ public class Item extends Card {
         collectableItems.add(item);
     }
 
-    public Item(String name , int cost , int MP , String desc , boolean collectable){
+    public Item(String name , int cost , int MP , String desc , boolean collectable , ArrayList<Buff> buffs , String effect){
 
         super(name,cost,MP);
         this.desc = desc;
         this.collectable = collectable;
+        for (Buff buff:buffs) {
+            this.buffs.add(buff);
+        }
+        this.effect = effect;
 
     }
 
