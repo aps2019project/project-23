6e4f331ced45148d2522ded4507e6522a9c1f5d5
Mohammad Shojaeyan <mshojaeyan@ce.sharif.edu>
2019-05-04 02:21:57 +0430

    Can allturn addMP

--- APProject/src/AddMP.java
+++ APProject/src/AddMP.java
@@ -3,20 +3,22 @@ import java.util.ArrayList;
 public class AddMP extends Buff {
 
     private ArrayList<Integer> addMP = new ArrayList<Integer>();
+    private boolean allTurnAddMP;
 
-    public AddMP(int turn, ArrayList<Integer> addMP) {
+    public AddMP(int turn, ArrayList<Integer> addMP, boolean allTurnAddMP) {
 
         super(false, true, false, false, turn);
         this.posOrNeg = false;
-        for ( int i = 0 ; i < addMP.size() ; i ++ ) {
+        for (int i = 0; i < addMP.size(); i++) {
             this.addMP.add(addMP.get(i));
         }
+        this.allTurnAddMP = allTurnAddMP;
 
     }
 
     public Buff copyBuff() {
 
-        AddMP addMP = new AddMP(turn, this.addMP);
+        AddMP addMP = new AddMP(turn, this.addMP,allTurnAddMP);
         return addMP;
 
     }
