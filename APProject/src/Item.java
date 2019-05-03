a1a11d04c1b0d551059086af9a0e41594bb5cb4f
Mohammad Shojaeyan <mshojaeyan@ce.sharif.edu>
2019-05-03 20:13:45 +0430

    Start to implement class item

--- APProject/src/Item.java
+++ APProject/src/Item.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
+
 public class Item extends Card {
 
     private String desc;
     private boolean collectable;
+    private static ArrayList<Item> collectableItems = new ArrayList<Item>();
+
+    public static ArrayList<Item> getCollectableItems() {
+        return collectableItems;
+    }
+
+    public static void addCollectableItem (Item item) {
+        collectableItems.add(item);
+    }
 
     public Item(String name , int cost , int MP , String desc , boolean collectable){
+
         super(name,cost,MP);
         this.desc = desc;
         this.collectable = collectable;
+
     }
 
     public String getDesc() {
