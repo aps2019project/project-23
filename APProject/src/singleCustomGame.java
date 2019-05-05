8f0263c5b3a4b1df6135b5bf640d5c048aa555fe
hosseinabadi <45671652+hosseinabadi@users.noreply.github.com>
2019-05-05 19:06:02 +0430

    set mode and deck of game

--- APProject/src/singleCustomGame.java
+++ APProject/src/singleCustomGame.java
@@ -1,10 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class singleCustomGame {
 
     public static void help() {
         System.out.println("start game [deck name] [mode] [number of flag (for mode 3)]");
     }
 
-    public static void menu(Account account){
+    public static void setGame(String deckName, String mode, String numberOfFlag) {
+
+
+    }
+
+    public static void menu(Account account) {
 
         System.out.println("Mode :");
         System.out.println("    1. Kill hero");
@@ -12,20 +20,31 @@ public class singleCustomGame {
         System.out.println("    3. Collect 1/2 of all flag");
         account.getCollection().showAllDecks();
         String command;
-        String [] commands;
-        while (true){
+        Matcher matcher;
+        Pattern startGamePat = Pattern.compile("^start game \\[(?<deckName>\\p{all}+)] \\[(?<mode>[0-9])]$");
+        Pattern startGameMode3 = Pattern.compile("^start game \\[(?<deckName>\\p{all}+)] \\[(?<mode>[0-9])] \\[(?<numberOfFlag>[0-9]+)]$");
+        while (true) {
 
             command = Main.getScanner().nextLine().trim().toLowerCase();
-            if (command.matches("help")){
+            if (command.matches("help")) {
                 help();
-            }
-            else if (command.matches("exit")){
+            } else if (command.matches("exit")) {
                 return;
             }
-            
+            matcher = startGamePat.matcher(command);
+            if (matcher.find()) {
+                setGame(matcher.group("deckName"), matcher.group("mode"), "none");
+
+            }
+
+            matcher = startGameMode3.matcher(command);
+            if (matcher.find()) {
+                setGame(matcher.group("deckName"), matcher.group("mode"), matcher.group("numberOfFlag"));
+                
+            }
+
         }
 
     }
 
 }
-
