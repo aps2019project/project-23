476d71c954c3954db98580023d31d44d58a1f649
hosseinabadi <45671652+hosseinabadi@users.noreply.github.com>
2019-05-06 01:47:36 +0430

    show info card

--- APProject/src/GameController.java
+++ APProject/src/GameController.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class GameController {
 
     public static void gameInfo(int mode, Deck player1Deck, Deck player2Deck) {
@@ -34,12 +38,68 @@ public class GameController {
 
     }
 
-    public static void control(Account account, Account account1, Deck player1Deck, Deck player2Deck, String command, int mode) {
+    public static Card getCardInCell(String cardID , ArrayList<Card> handPlayer1) {
+
+        for ( int i = 0 ; i < 9; i ++ ) {
+            for ( int j = 0 ; j < 5 ; j ++ ) {
+                if (Main.getCardsCell()[i][j].cardID.matches(cardID)){
+                    return Main.getCardsCell()[i][j];
+                }
+            }
+        }
+
+        for ( int i = 0 ; i < handPlayer1.size() ; i ++ ) {
+            if (handPlayer1.get(i).cardID.matches(cardID)){
+                return handPlayer1.get(i);
+            }
+        }
+        return null;
+
+    }
+
+    public static void showCardInfo(String cardID,ArrayList<Card> handPlayer1) {
+
+        if (getCardInCell(cardID,handPlayer1) == null) {
+            System.out.printf("Don't exist this card");
+            return;
+        }
+        Card card = getCardInCell(cardID,handPlayer1);
+
+        if ( card instanceof Hero ) {
+            System.out.println("Hero :");
+            System.out.printf("     Name : %s\n" , card.name);
+            System.out.printf("Cost : %d\n" , card.cost);
+            System.out.printf("Desc : %s\n" , ((Hero) card).getSpecialPower());
+            System.out.printf("MP : %d\n" , card.MP);
+            System.out.printf("");
+        }
+        else if (card instanceof Minion){
+
+        }
+        else {
+
+        }
+
+    }
+
+    public static void control(Account account, Account account1, Deck player1Deck, Deck player2Deck, ArrayList<Card> handPlayer1, String command, int mode) {
+
+        Pattern showCardInfoPat = Pattern.compile("^show card info \\[(?<cardID>\\p{all}+)]$");
+        Matcher matcher;
 
         if (command.matches("game info")) {
             gameInfo(mode, player1Deck, player2Deck);
         } else if (command.matches("show my minions")) {
             showMyMinion(account);
+        }
+        else if (command.matches("show opponent minions")) {
+            showMyMinion(account1);
+        }
+
+        matcher = showCardInfoPat.matcher(command);
+        if (matcher.find()){
+            showCardInfo(matcher.group("cardID"),handPlayer1);
+            return;
         }
 
     }
