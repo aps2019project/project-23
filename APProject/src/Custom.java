7b6fc95dc7572f2813d42b488f368bca6e5ea51f
hosseinabadi <45671652+hosseinabadi@users.noreply.github.com>
2019-05-05 23:55:39 +0430

    custom finish

--- APProject/src/Custom.java
+++ APProject/src/Custom.java
@@ -1,10 +1,14 @@
 public class Custom {
 
-    private static Deck player1Deck;
-    private static Deck player2Deck;
+    protected static Deck player1Deck;
+    protected static Deck player2Deck;
 
+    public static void setPlayer1Deck(Deck player1Deck) {
+        Custom.player1Deck = player1Deck;
+    }
 
-
-
+    public static void setPlayer2Deck(Deck player2Deck) {
+        Custom.player2Deck = player2Deck;
+    }
 
 }
