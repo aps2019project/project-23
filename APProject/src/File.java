96cdf310073f638ff9afbec8f41e97da23d2f0b6
alimohammadiamirhossein <45646480+alimohammadiamirhossein@users.noreply.github.com>
2019-05-06 20:15:02 +0430

    save collection and money

--- APProject/src/File.java
+++ APProject/src/File.java
@@ -8,17 +8,35 @@ public class File {
         try {
             FileWriter fileWriter = new FileWriter("/home/shajusoni/IdeaProjects/APProject/account.txt");
             for (Account account : accounts) {
-                fileWriter.write("Username : " + account.getUsername() + "\n");
-                fileWriter.write("Password : " + account.getPassword() + "\n");
-                fileWriter.write("Money : " + account.getBudget() + "\n");
-                fileWriter.write("Match history :\n");
+                fileWriter.write("Username:" + account.getUsername() + "\n");
+                fileWriter.write("Password:" + account.getPassword() + "\n");
+                fileWriter.write("Money:" + account.getBudget() + "\n");
+                fileWriter.write("MatchHistory:\n");
                 if (account.getAllMatches().size() == 0) {
-                    fileWriter.write("      Don't exist any matches\n");
+                    fileWriter.write("None\n");
                 } else {
                     for (Match match : account.getAllMatches()) {
-                        fileWriter.write("      " + account.getAllMatches().indexOf(match) + ". ");
-                        fileWriter.write(match.getNameOfOpponent() + " " + match.getWinOrLose() + " " + match.getTimeOfMatch());
+                        fileWriter.write(account.getAllMatches().indexOf(match) + ".");
+                        fileWriter.write("name:" + match.getNameOfOpponent() + "win:" + match.getWinOrLose() + "time:" + match.getTimeOfMatch());
+                        fileWriter.write("\n");
                     }
+                }
+                fileWriter.write("Collection:\n");
+                if (account.getCollection().getAllCards().size() == 0) {
+                    fileWriter.write("None\n");
+                } else {
+                    for (Card card : account.getCollection().getAllCards()) {
+                        fileWriter.write(account.getCollection().getAllCards().indexOf(card) + ".");
+                        fileWriter.write(card.name);
+                        fileWriter.write(" ");
+                    }
+                    fileWriter.write("\n");
+                }
+                fileWriter.write("Decks:\n");
+                if (account.getCollection().getAllDecks().size() == 0) {
+                    fileWriter.write("None\n");
+                } else {
+                    
                 }
 
             }
