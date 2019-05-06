1651f5ac1cf9bc21ce50114304008be58816689f
alimohammadiamirhossein <45646480+alimohammadiamirhossein@users.noreply.github.com>
2019-05-06 06:06:52 +0430

    set selected card

--- APProject/src/GameController.java
+++ APProject/src/GameController.java
@@ -38,18 +38,18 @@ public class GameController {
 
     }
 
-    public static Card getCardInCell(String cardID , ArrayList<Card> handPlayer1) {
+    public static Card getCardInCell(String cardID, ArrayList<Card> handPlayer1) {
 
-        for ( int i = 0 ; i < 9; i ++ ) {
-            for ( int j = 0 ; j < 5 ; j ++ ) {
-                if (Main.getCardsCell()[i][j].cardID.matches(cardID)){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (Main.getCardsCell()[i][j].cardID.matches(cardID)) {
                     return Main.getCardsCell()[i][j];
                 }
             }
         }
 
-        for ( int i = 0 ; i < handPlayer1.size() ; i ++ ) {
-            if (handPlayer1.get(i).cardID.matches(cardID)){
+        for (int i = 0; i < handPlayer1.size(); i++) {
+            if (handPlayer1.get(i).cardID.matches(cardID)) {
                 return handPlayer1.get(i);
             }
         }
@@ -57,51 +57,101 @@ public class GameController {
 
     }
 
-    public static void showCardInfo(String cardID,ArrayList<Card> handPlayer1) {
+    public static void showCardInfo(String cardID, ArrayList<Card> handPlayer1) {
 
-        if (getCardInCell(cardID,handPlayer1) == null) {
+        if (getCardInCell(cardID, handPlayer1) == null) {
             System.out.printf("Don't exist this card");
             return;
         }
-        Card card = getCardInCell(cardID,handPlayer1);
+        Card card = getCardInCell(cardID, handPlayer1);
 
-        if ( card instanceof Hero ) {
+        if (card instanceof Hero) {
             System.out.println("Hero :");
-            System.out.printf("     Name : %s\n" , card.name);
-            System.out.printf("Cost : %d\n" , card.cost);
-            System.out.printf("Desc : %s\n" , ((Hero) card).getSpecialPower());
-            System.out.printf("MP : %d\n" , card.MP);
-            System.out.printf("");
-        }
-        else if (card instanceof Minion){
-
-        }
-        else {
-
+            System.out.printf("     Name : %s\n", card.name);
+            System.out.printf("     Cost : %d\n", card.cost);
+            System.out.printf("     Desc : %s\n", ((Hero) card).getSpecialPower());
+            System.out.printf("     MP : %d\n", card.MP);
+            System.out.printf("     HP : %d\n", ((Hero) card).getHP());
+            System.out.printf("     AP : %d\n", ((Hero) card).getAP());
+        } else if (card instanceof Minion) {
+            System.out.println("Minion :");
+            System.out.printf("     Name : %s\n", card.name);
+            System.out.printf("     Cost : %d\n", card.cost);
+            System.out.printf("     HP : %d AP : %d MP : %d\n", ((Minion) card).getHP(), ((Minion) card).getAP(), card.MP);
+            System.out.printf("     Desc : %s\n", ((Minion) card).getSpecialPower());
+            System.out.printf("     Combo-ability : ");
+            if (((Minion) card).getSpecialPower().matches("combo")) {
+                System.out.printf("Have\n");
+            } else {
+                System.out.printf("Don't have");
+            }
+            System.out.printf("     Range : %d\n", ((Minion) card).getAttackRange());
+        } else if (card instanceof Spell) {
+            System.out.println("Spell : ");
+            System.out.printf("     Name : %s\n", card.name);
+            System.out.printf("     MP : %d\n", card.MP);
+            System.out.printf("     Cost : %d", card.cost);
+            System.out.printf("     Desc : %s\n", ((Spell) card).getDesc());
         }
 
     }
 
-    public static void control(Account account, Account account1, Deck player1Deck, Deck player2Deck, ArrayList<Card> handPlayer1, String command, int mode) {
+    public static void selectCard(String cardID, Account account) {
+
+        boolean existCard = false;
+        int x = 0, y = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (Main.getCardsCell()[i][j].cardID.matches(cardID)) {
+                    existCard = true;
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        if (!existCard) {
+            System.out.println("Invalid card");
+            return;
+        }
+        if (Main.getCardsCell()[x][y].numberOfPlayer != account.getNumberOfPlayer()) {
+            System.out.println("This card is not for you");
+            return;
+        }
+        if (account.getNumberOfPlayer() == 1) {
+            KillMode.setSelectedCard1(Main.getCardsCell()[x][y]);
+        } else if (account.getNumberOfPlayer() == 2) {
+            KillMode.setSelectedCard2(Main.getCardsCell()[x][y]);
+        }
+
+    }
+
+    public static void control(Account account, Account account1, String command, int mode) {
 
         Pattern showCardInfoPat = Pattern.compile("^show card info \\[(?<cardID>\\p{all}+)]$");
+        Pattern selectPat = Pattern.compile("^select \\[(?<cardID>)\\p{all}+]$");
         Matcher matcher;
 
         if (command.matches("game info")) {
-            gameInfo(mode, player1Deck, player2Deck);
+            gameInfo(mode, KillMode.player1Deck, KillMode.player2Deck);
         } else if (command.matches("show my minions")) {
             showMyMinion(account);
-        }
-        else if (command.matches("show opponent minions")) {
+        } else if (command.matches("show opponent minions")) {
             showMyMinion(account1);
         }
 
         matcher = showCardInfoPat.matcher(command);
-        if (matcher.find()){
-            showCardInfo(matcher.group("cardID"),handPlayer1);
+        if (matcher.find()) {
+            showCardInfo(matcher.group("cardID"), KillMode.getHandPlayer1());
+            return;
+        }
+
+        matcher = selectPat.matcher(command);
+        if (matcher.find()) {
+            selectCard(matcher.group("cardID"), account);
             return;
         }
 
     }
 
 }
+
