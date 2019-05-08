5c042cfaabe978f316410ab0536f848bd62f93cc
hosseinabadi <45671652+hosseinabadi@users.noreply.github.com>
2019-05-08 04:31:05 +0430

    game info in game controller

--- APProject/src/GameController.java
+++ APProject/src/GameController.java
@@ -30,8 +30,17 @@ public class GameController {
     public static void gameInfo(int mode, Deck player1Deck, Deck player2Deck) {
 
         if (mode == 1) {
-            System.out.printf("HP of Hero of Player_1 : %d\n", ((Hero) player1Deck.getDeckCard().get(player1Deck.indexOfHero())).getHP());
-            System.out.printf("HP of Hero of Player_2 : %d\n", ((Hero) player2Deck.getDeckCard().get(player2Deck.indexOfHero())).getHP());
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (Main.getCardsCell()[i][j] instanceof Hero) {
+                        if (Main.getCardsCell()[i][j].numberOfPlayer == 1) {
+                            System.out.printf("HP of Hero of Player_1 : %d\n", ((Hero) Main.getCardsCell()[i][j]).getHP());
+                        } else {
+                            System.out.printf("HP of Hero of Player_2 : %d\n", ((Hero) Main.getCardsCell()[i][j]).getHP());
+                        }
+                    }
+                }
+            }
         } else if (mode == 2) {
 
         } else {
@@ -44,15 +53,18 @@ public class GameController {
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 5; j++) {
+                if (Main.getCardsCell()[i][j] == null) {
+                    continue;
+                }
                 if (Main.getCardsCell()[i][j].numberOfPlayer == account.getNumberOfPlayer()) {
                     System.out.printf("cardID : %s, ", Main.getCardsCell()[i][j].cardID);
                     if (Main.getCardsCell()[i][j] instanceof Hero) {
                         System.out.printf("health : %d, ", ((Hero) Main.getCardsCell()[i][j]).getHP());
-                        System.out.printf("location : [%d,%d], ", i, j);
+                        System.out.printf("location : [%d,%d], ", i+1, j+1);
                         System.out.printf("power : %d\n", ((Hero) Main.getCardsCell()[i][j]).getAP());
                     } else if (Main.getCardsCell()[i][j] instanceof Minion) {
                         System.out.printf("health : %d, ", ((Minion) Main.getCardsCell()[i][j]).getHP());
-                        System.out.printf("location : [%d,%d], ", i, j);
+                        System.out.printf("location : [%d,%d], ", i+1, j+1);
                         System.out.printf("power : %d\n", ((Minion) Main.getCardsCell()[i][j]).getAP());
                     }
                 }
@@ -65,7 +77,7 @@ public class GameController {
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 5; j++) {
-                if (Main.getCardsCell()[i][j].cardID.matches(cardID)) {
+                if (Main.getCardsCell()[i][j] != null && Main.getCardsCell()[i][j].cardID.matches(cardID)) {
                     return Main.getCardsCell()[i][j];
                 }
             }
@@ -113,7 +125,7 @@ public class GameController {
             System.out.println("Spell : ");
             System.out.printf("     Name : %s\n", card.name);
             System.out.printf("     MP : %d\n", card.MP);
-            System.out.printf("     Cost : %d", card.cost);
+            System.out.printf("     Cost : %d\n", card.cost);
             System.out.printf("     Desc : %s\n", ((Spell) card).getDesc());
         }
 
@@ -125,6 +137,9 @@ public class GameController {
         int x = 0, y = 0;
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 5; j++) {
+                if (Main.getCardsCell()[i][j] == null) {
+                    continue;
+                }
                 if (Main.getCardsCell()[i][j].cardID.matches(cardID)) {
                     existCard = true;
                     x = i;
