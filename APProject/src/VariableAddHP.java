3b9544a59db2c4c62bd32d211bdaafa09264b1bd
Mohammad Shojaeyan <mshojaeyan@ce.sharif.edu>
2019-05-17 03:55:06 +0430

    change variable add HP

--- APProject/src/VariableAddHP.java
+++ APProject/src/VariableAddHP.java
@@ -4,10 +4,10 @@ public class VariableAddHP extends Buff {
 
     private ArrayList<Integer> addHP = new ArrayList<Integer>();
 
-    public VariableAddHP(boolean allTurnEffect, boolean allTurn, boolean continuous, int turn , ArrayList<Integer> addHP){
+    public VariableAddHP(boolean allTurnEffect, boolean allTurn, boolean continuous, int turn, ArrayList<Integer> addHP) {
 
-        super(false,allTurnEffect,allTurn,continuous,turn);
-        for ( int i = 0 ; i < addHP.size() ; i ++ ) {
+        super(false, allTurnEffect, allTurn, continuous, turn);
+        for (int i = 0; i < addHP.size(); i++) {
             this.addHP.add(addHP.get(i));
         }
 
@@ -22,6 +22,13 @@ public class VariableAddHP extends Buff {
     }
 
     public void effectBuffsOnCard(Card card, int numberOfPlayer) {
+        if (card instanceof Hero) {
+            ((Hero) card).addHP(addHP.get(0) * -1);
+            addHP.remove(0);
+        } else if (card instanceof Minion) {
+            ((Minion) card).addHP(addHP.get(0) * -1);
+            addHP.remove(0);
+        }
     }
 
 }
